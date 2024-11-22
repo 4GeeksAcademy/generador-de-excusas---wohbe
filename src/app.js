bb3dc0a5f2f0d7c4887d67bd2822108565d51e70
Roberto <robercantalejo@gmail.com>
2024-11-22 11:45:39 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generador() {
+  let who = ["The cat", "The bus driver", "My aunt", "A rat"];
+
+  let action = ["stole", "broke", "lost", "threw"];
+
+  let what = ["my homework", "my laptop", "the ID card"];
+
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whofr = Math.floor(Math.random() * who.length);
+
+  let actionfr = Math.floor(Math.random() * action.length);
+
+  let whatfr = Math.floor(Math.random() * what.length);
+
+  let whenfr = Math.floor(Math.random() * when.length);
+
+  document.querySelector("#excuse").innerHTML =
+    who[whofr] +
+    " " +
+    action[actionfr] +
+    " " +
+    what[whatfr] +
+    " " +
+    when[whenfr] +
+    "!";
+}
+
+document.querySelector("#boton").addEventListener("click", generador);
